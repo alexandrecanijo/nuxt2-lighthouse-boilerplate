e0ea44889918b39db7225f18fd858c415c0e1d1a
Alexandre Canijo <alex@alexandrecanijo.com>
2022-09-08 02:43:18 +0100

    chore: trying to fix performance score

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,11 +21,18 @@ export default {
       },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'preconnect',
+        href: 'https://cdn.jsdelivr.net',
+        crossorigin: 'crossorigin',
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@assets/styles/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -39,11 +46,108 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/style-resources',
   ],
+
+  styleResources: {
+    hoistUseStatements: true,
+    scss: ['~/assets/styles/scss/_variables.scss'],
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
 
+  loading: false,
+
+  loadingIndicator: false,
+
+  fetch: {
+    client: false,
+    server: false,
+  },
+
+  features: {
+    store: false,
+    middleware: false,
+    transitions: false,
+    deprecations: false,
+    validate: false,
+    asyncData: false,
+    fetch: false,
+    clientOnline: false,
+    clientPrefetch: false,
+    clientUseUrl: true,
+    componentAliases: false,
+    componentClientOnly: false,
+  },
+
+  render: {
+    http2: {
+      push: true,
+      pushAssets: (req, res, publicPath, preloadFiles) =>
+        preloadFiles.map(
+          (f) => `<${publicPath}${f.file}>; rel=preload; as=${f.asType}`
+        ),
+    },
+    asyncScripts: true,
+    resourceHints: true,
+    ssr: true,
+    ssrLog: true,
+    static: {
+      // 97 days, minimum to pass lighthouse scores - https://github.com/GoogleChrome/lighthouse/issues/11380
+      maxAge: 8380800000,
+    },
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    indicator: false,
+    terser: true,
+    postcss: null,
+    filenames: {
+      app: ({ isDev }) => (isDev ? '[name].js' : '[contenthash].js'),
+      chunk: ({ isDev }) => (isDev ? '[name].js' : '[contenthash].js'),
+      css: ({ isDev }) => (isDev ? '[name].css' : '[contenthash].css'),
+      img: ({ isDev }) =>
+        isDev ? '[path][name].[ext]' : 'img/[contenthash:7].[ext]',
+      font: ({ isDev }) =>
+        isDev ? '[path][name].[ext]' : 'fonts/[contenthash:7].[ext]',
+      video: ({ isDev }) =>
+        isDev ? '[path][name].[ext]' : 'videos/[contenthash:7].[ext]',
+    },
+    quiet: false,
+    optimization: {
+      splitChunks: {
+        name: true,
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.(css|vue)$/,
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
+    },
+    html: {
+      minify: {
+        collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true,
+        html5: true,
+        removeComments: true,
+      },
+    },
+    extractCSS: true,
+  },
+
+  generate: {
+    fallback: true,
+  },
 }
